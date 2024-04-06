a3d9d4fa2bb8210d119f9c416f10bf5881304a3b
gauravghodinde <gauravghodinde@gmail.com>
2024-04-07 01:01:04 +0530

    added ipo and ipo/companyid

---
+++ pages/ipo.jsx
@@ -0,0 +1,21 @@
+
+// import NFTGrid from  "../components/NFT/NFTGrid";//"../components/NFTGrid";
+import { NFT_COLLECTION_ADDRESSS } from  "../const/contractAddresses";//"../consts/addresses";
+import { useContract, useNFTs } from "@thirdweb-dev/react";
+import { useEffect } from 'react';
+import CompanyCollection from  "../components/campanyCollections" ;// '../components/companyCollections';
+
+  const Ipo=()=> {
+    return (
+        <div >
+            <div className="title">Ipo</div>
+            <div className="compContainer" >
+                {NFT_COLLECTION_ADDRESSS.map((company, i) =>
+                    <CompanyCollection company={company} key={i} />
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default Ipo
---
+++ pages/ipo/[CompanyCollectionAddress].jsx
@@ -0,0 +1,162 @@
+import {
+  MediaRenderer,
+  ThirdwebNftMedia,
+  useAddress,
+  useContract,
+  
+  useContractEvents,
+  useActiveClaimCondition,
+  useClaimNFT,
+  useValidDirectListings,
+  useValidEnglishAuctions,
+  Web3Button
+} from "@thirdweb-dev/react"
+import { createThirdwebClient , getContract } from "thirdweb";
+import React, { useEffect, useState } from "react"
+import Container from "../../components/Container/Container"
+import { ThirdwebSDK } from "@thirdweb-dev/sdk"
+import {
+  ETHERSCAN_URL,
+  MARKETPLACE_ADDRESS,
+  NETWORK,
+  NFT_COLLECTION_ADDRESS,
+  NFT_COLLECTION_ADDRESSS
+} from "../../const/contractAddresses"
+import randomColor from "../../util/randomColor"
+import Skeleton from "../../components/Skeleton/Skeleton"
+import toast, { Toaster } from "react-hot-toast"
+import toastStyle from "../../util/toastConfig"
+import { sepolia } from "thirdweb/chains";
+const [randomColor1, randomColor2] = [randomColor(), randomColor()]
+import { prepareContractCall, resolveMethod } from "thirdweb"
+import { useSendTransaction } from "@thirdweb-dev/react";
+import { useRouter } from 'next/router';
+
+
+//-----------------------------------------------
+
+
+export default function TokenPage({ nft, contractMetadata }) {
+  const router = useRouter();
+  const { CompanyCollectionAddress } = router.query;
+  const client = createThirdwebClient({ 
+    clientId: "87407747d9b3db5ef79631fa33233ab1" 
+  });
+  const { contract } = useContract(CompanyCollectionAddress);
+  const [metadata, setMetadata] = useState(null);
+  const wallet_address = useAddress()
+  
+  // claming nfts
+  const {
+    mutateAsync: claimNft,
+    isLoading,
+    error,
+  } = useClaimNFT(contract);
+
+  if (error) {
+    console.error("failed to claim nft", error);
+  }
+  const { data, isLoading : loading, error: errorof } =  useActiveClaimCondition(contract);
+  
+  // claming nfts
+  
+  // metadata of stock
+  useEffect(() => {
+      getMetaData();
+    }, [contract, metadata])
+
+    const getMetaData = async () => {
+      let metadata = await contract?.metadata.get();
+      setMetadata(metadata);
+    }
+    // metadata of stock
+    
+    useEffect(()=>{
+      if(!loading){
+        console.log(data)
+      }
+    },[data])
+    
+    return (
+    <>
+    <div style={{display:"flex",justifyContent:"center", alignItems:"center", marginTop:50}}>
+      {/* {console.log(docum)} */}
+      <Toaster position="bottom-center" reverseOrder={false} />
+      <Container maxWidth="lg">
+        {metadata && <ThirdwebNftMedia metadata={metadata} height={100}
+                        width={100}/>}
+                        <h1>{metadata?.name}</h1>
+                        {/* <p>{metadata?.description}</p> */}
+        <div>Start Time  --- {data?.startTime.toDateString()}</div>
+        <div>Price ------- {data?.currencyMetadata.displayValue}</div>
+        <div>Stocks Left ---- {data?.availableSupply}</div>    
+        <div>limit -----------{data?.maxClaimablePerWallet}</div>
+        <div>issue size -- {Number(data?.maxClaimableSupply) * Number(data?.currencyMetadata.displayValue)}</div> 
+      <Web3Button
+      contractAddress={CompanyCollectionAddress}
+      action={() =>
+        claimNft({
+          to: wallet_address, // Use useAddress hook to get current wallet address
+          quantity: 1,
+        })
+      }
+    >
+      Claim NFT
+    </Web3Button>
+        
+      </Container>
+      </div>
+    </>
+  )
+}
+
+export const getStaticProps = async context => {
+  const CompanyCollectionAddress = context.params?.CompanyCollectionAddress
+
+  const sdk = new ThirdwebSDK(NETWORK, {
+    secretKey: process.env.TW_SECRET_KEY
+  })
+
+  const contract = await sdk.getContract(CompanyCollectionAddress)
+
+  // const nft = await contract.erc721.get(CompanyCollectionAddress)
+
+  let contractMetadata
+
+  try {
+    contractMetadata = await contract.metadata.get()
+  } catch (e) {}
+
+  return {
+    props: {
+      // nft,
+      contractMetadata: contractMetadata || null
+    },
+    revalidate: 1 // https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration
+  }
+}
+
+export const getStaticPaths = async () => {
+  const sdk = new ThirdwebSDK(NETWORK, {
+    secretKey: process.env.TW_SECRET_KEY
+  })
+
+  const contract = await sdk.getContract(NFT_COLLECTION_ADDRESS)
+
+
+  const collects = NFT_COLLECTION_ADDRESSS
+  const paths = collects.map(address => {
+    console.log(address.address)
+    return {
+      params: {
+        CompanyCollectionAddress:address.address,
+        
+      }
+    }
+  })
+
+  return {
+    paths,
+    fallback: "blocking" // can also be true or 'blocking'
+  }
+}
