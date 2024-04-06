4e6ba0fb5917890eb5ec6549ca876c27eb822fa8
karanagarwal12 <karanagarwal200505@gmail.com>
2024-04-07 00:51:00 +0530

    card skeleton added

--- components/campanyCollections.jsx
+++ components/campanyCollections.jsx
@@ -1,7 +1,9 @@
 import { useContract, useNFTs, ThirdwebNftMedia } from "@thirdweb-dev/react";
 import styles from '../styles/Marketplace.module.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Link from "next/link";
+import Skeleton from '../components/Skeleton/Skeleton';
+
 export default function CompanyCollection({ company }) {
     const { contract } = useContract(company?.address);
     const [metadata, setMetadata] = useState(null);
@@ -12,34 +14,41 @@ export default function CompanyCollection({ company }) {
         let metadata = await contract?.metadata.get();
         setMetadata(metadata);
     }
+
     return (
-        <Link
-            href={`/token/${company?.address}`}
-            key={company}
-        >
-            <div className={styles.companyCollection}>
-                {metadata &&
-                    <div className={styles.company}>
-                        <ThirdwebNftMedia
-                            metadata={metadata}
-                            height={"18vw"}
-                            width={"18vw"}
-                        />
-                        <div className={styles.companyName}>{company?.name}</div>
-                        <div className={styles.details}>
-                            <div className={styles.detail}>
-                                <span>Price</span>
-                                <span className="currentMarketPrice">0.0002 ETH</span>
-                            </div>
-                            <div className={styles.detail}>
-                                <span>Change</span>
-                                <span>+0.00002 (10%)</span>
+        <>
+            {metadata &&
+                <Link
+                    href={`/token/${company?.address}`}
+                    key={company}
+                >
+                    <div className={styles.companyCollection}>
+                        <div className={styles.company}>
+                            <ThirdwebNftMedia
+                                metadata={metadata}
+                                height={"18vw"}
+                                width={"18vw"}
+                            />
+                            <div className={styles.companyName}>{company?.name}</div>
+                            <div className={styles.details}>
+                                <div className={styles.detail}>
+                                    <span>Price</span>
+                                    <span className="currentMarketPrice">0.0002 ETH</span>
+                                </div>
+                                <div className={styles.detail}>
+                                    <span>Change</span>
+                                    <span>+0.00002 (10%)</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    || <div>Loading...</div>
-                }
-            </div></Link>
+                    </div></Link>
+                || <Skeleton
+                    width="20vw"
+                    height="27vw"
+                    borderRadius="1vw"
+                />
+            }
+        </>
 
     );
 }
