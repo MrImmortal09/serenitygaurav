427ba580065ddf19b3b281ecb23549ca33018b05
karanagarwal12 <karanagarwal200505@gmail.com>
2024-04-07 06:14:12 +0530

    added more companies to our website

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -13,18 +13,30 @@ export const MARKETPLACE_ADDRESS = "0x2F1aE405DbA52510E657878D78Dc5Fb9239395D6";
 export const NFT_COLLECTION_ADDRESS =
   "0x8e11eC4f93F0339a804922eabE933257C0e1c761";
 
-export const TESLA_COLLECTION_ADDRESS =  "0xb17a70a037e3EBa5962Cf57b8aE7fc1e1Ea1d6a2";
+export const TESLA_COLLECTION_ADDRESS = "0xb17a70a037e3EBa5962Cf57b8aE7fc1e1Ea1d6a2";
 
 export const ETHERSCAN_URL = "https://sepolia.etherscan.io/";
 
 
 export const NFT_COLLECTION_ADDRESSS = [
   {
-      name: "Mercedes",
-      address: "0x2f7a904E7b3915547D4a83BCF2Ef5011AaFb3400",
+    name: "Mercedes",
+    address: "0x2f7a904E7b3915547D4a83BCF2Ef5011AaFb3400",
   },
   {
-      name: "Tesla",
-      address: "0xb17a70a037e3EBa5962Cf57b8aE7fc1e1Ea1d6a2",
+    name: "Tesla",
+    address: "0xb17a70a037e3EBa5962Cf57b8aE7fc1e1Ea1d6a2",
+  },
+  {
+    name: "Intel",
+    address: "0x2A3d076944D19B8BCad83282AeCd6A238d6dbB41",
+  },
+  {
+    name: "Nvidia",
+    address: "0x58EDAaaBcc60c5DF7D5FF73355cbB6918E5674Ec",
+  },
+  {
+    name: "Adobe",
+    address: "0xEE040Dab1679df772A34A4828982baFFaABc9BfB",
   }
 ]
